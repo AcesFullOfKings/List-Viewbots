0a58070430f38eaae569f50fe07aedf197c8027d
Theonefoster <Theonefoster@users.noreply.github.com>
2020-05-13 23:21:29 +0100

    Update List Viewbots.py

--- List Viewbots.py
+++ List Viewbots.py
@@ -1,20 +1,18 @@
 import requests
 import ctypes
 
-from itertools import combinations
-from difflib import SequenceMatcher
-from time import sleep
-from os import path
 from contextlib import suppress
-
+from itertools  import combinations
+from difflib    import SequenceMatcher
+from time       import sleep
+from os         import path
 
 ctypes.windll.kernel32.SetConsoleTitleW("Viewbot Detector") # set window name
 
-similarity = lambda a, b: SequenceMatcher(None, a, b).ratio()
+similarity = lambda a, b: SequenceMatcher(None, a, b).ratio() # function to compare how similar two strings are in %
 
 views_url = ""
-
-bound = 0
+threshold = 0
 
 if path.exists("config.txt"):
 	with open("config.txt", "r") as f:
@@ -22,8 +20,8 @@ if path.exists("config.txt"):
 	for line in file.split("\n"):
 		with suppress(ValueError):
 			key, val = line.split("=")
-			if key.replace(" ", "") == "bound" and 0 < int(val) < 100: # ValueError if val not formatted properly (eg is str), will be suppressed and line ignored
-				bound = int(val) 
+			if key.replace(" ", "") == "threshold" and 0 < int(val) < 100: # ValueError if val not formatted properly (eg is str), will be suppressed and line ignored
+				threshold = int(val) 
 				break
 			elif key == "channel" and 0 < len(str(val)) <= 20 and val.lower()!="valkia":
 				views_url = "https://tmi.twitch.tv/group/user/{channel}/chatters".format(channel=val)
@@ -32,16 +30,16 @@ if path.exists("config.txt"):
 if views_url == "":
 	views_url = "https://tmi.twitch.tv/group/user/valkia/chatters"
 	print("Using channel Valkia (default)")
-if bound == 0:
-	bound = 75
-	print("Minimum similarity is {x}% (default value)".format(x=bound))
+if threshold == 0:
+	threshold = 75
+	print("Minimum similarity is {x}% (default value)".format(x=threshold))
 else:
-	print("Minimum similarity is {x}% (read from config)".format(x=bound))
+	print("Minimum similarity is {x}% (read from config)".format(x=threshold))
 
 viewers = requests.get(views_url).json()["chatters"]["viewers"]
 name_pairs = list(combinations(viewers, 2))
 
-bound = bound / 100
+threshold = threshold / 100
 
 print("There are {n} viewers in the list. Searching for similar names..\n\n".format(n=len(viewers)))
 
@@ -49,7 +47,7 @@ found = False
 
 for a,b in name_pairs:
 	match = similarity(a,b)
-	if match >= bound:
+	if match >= threshold:
 		found = True
 		print("{a} | {b} ({x}% match)".format(a=a.rjust(15), b=b.ljust(15), x=round(match*100, 1)))
 
